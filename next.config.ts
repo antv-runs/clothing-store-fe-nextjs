714147715938d18116ce24664ea92554c2c48c7f
AnhAnNek <info.vananne@gmail.com>
2026-07-19 23:08:36 +0700

    fix(sass, gallery): resolve sass compile error on Windows and fix thumbnail alignment misalignment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.join(__dirname),
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+  },
 };
 
 export default nextConfig;
+
